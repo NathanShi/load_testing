6e9c1b57de16d7890051d73eadb0ad254a26c3b4
nathanshi <yibing.shi@snapsheet.me>
2018-09-14 14:31:33 -0500

    add shell script

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # load_testing
-Shell script for quickly running `apache benchmark`, `siege`, `wrk` for load testing
+Shell script for quickly running `apache benchmark`, `siege` for load testing
+
+# Setup
+
+`git clone https://github.com/NathanShi/load_testing.git`
+
+# Usage
+
+`sh benchmark.sh`
---
+++ benchmark.sh
@@ -0,0 +1,60 @@
+#!/usr/bin bash
+
+# -------------------------------
+# Load Tests with different tools
+# - Apache Benchmark: built in for mac, https://httpd.apache.org/docs/2.4/programs/ab.html
+# - Siege: `brew install siege`, https://github.com/JoeDog/siege
+# - weighttp: could also be used, but it's not supporting ssl/https.
+
+DIVIDER="\n--------------------------\n--------------------------\n"
+if [ ! -d "benchmark_result" ]
+then
+  mkdir "./benchmark_result"
+fi
+
+# Settings:
+SITE="https://www.google.com/"
+OUTFILES="./benchmark_result/ab_result.txt ./benchmark_result/siege_result.txt"
+TIMEOUT=200
+# TOTAL_REQUEST: For each round, how many request in total you would like to load?
+TOTAL_REQUEST=20
+# CONCURRENT_USER_ARRAY: For each round, how many concurrent user you would like to simulate?
+CONCURRENT_USER_ARRAY=( 1 5 10 20 )
+# --- Other settings:
+# AUTH=""         # add '-A' tag for ab
+# HTTP_METHOD=""  # add '-m' tag
+# CONTENT_TYPE="" # add '-T' tag
+# HEADER=""       # add '-H' tag for siege
+# PUT_BODY=""     # add '-u' tag
+# POST_BODY=""    # add '-p' tag
+# ---
+
+# Benchmark START:
+echo "Benching: $SITE"
+for FILE in $OUTFILES; do
+  touch $FILE
+  echo "Benching: $SITE" > $FILE
+  echo $DIVIDER >> $FILE
+done
+
+for round in ${!CONCURRENT_USER_ARRAY[@]}; do
+  echo "Round $(( $round + 1 )): "
+  echo "${CONCURRENT_USER_ARRAY[round]} concurrent users doing $TOTAL_REQUEST page hits."
+
+  for FILE in $OUTFILES; do
+    echo "Round $(( $round + 1 )): " >> $FILE
+    echo "" >> $FILE
+    echo "Result of $TOTAL_REQUEST page loads by ${CONCURRENT_USER_ARRAY[round]} concurrent users." >> $FILE
+    echo "" >> $FILE
+  done
+
+  # apache benchmark
+  ab -r -n $TOTAL_REQUEST -c ${CONCURRENT_USER_ARRAY[round]} -s $TIMEOUT $SITE >> "./benchmark_result/ab_result.txt"
+
+  # siege
+  siege -c${CONCURRENT_USER_ARRAY[round]} -r$((TOTAL_REQUEST / ${CONCURRENT_USER_ARRAY[round]})) $SITE >> "./benchmark_result/siege_result.txt"
+
+  for FILE in $OUTFILES; do
+    echo $DIVIDER >> $FILE
+  done
+done
